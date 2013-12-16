d4cc0a02de063b112f8261165f65a992a23d21a5
Dan Friel <ddfriel@gmail.com>
2013-12-16 10:30:55 -0600

    on 155

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,10 +1,28 @@
-    i = 1
-    result = 1
-    while true
-      break unless i <= 10
-      result = result * i
-      i += 1
+  def test_rescue_clause
+    result = nil
+    begin
+      fail "Oops"
+    rescue StandardError => ex
+      result = :exception_handled
     end
 
-    puts result
-    
+  puts result
+
+puts ex.is_a?(StandardError)
+
+puts RuntimeError.ancestors.include?(StandardError)
+
+puts ex.message
+    #assert_equal :exception_handled, result
+
+    #assert_equal "Should be a Standard Error", ex.is_a?(StandardError), "Should be a Standard Error"
+    #assert_equal nil, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
+
+    #assert RuntimeError.ancestors.include?(StandardError),
+    #  "RuntimeError is a subclass of StandardError"
+
+    #@assert_equal __, ex.message
+  end
+
+test_rescue_clause
+  
